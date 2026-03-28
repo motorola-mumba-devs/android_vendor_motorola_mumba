13d86ea8249e98ba80422023f28f4f6e01ff4a5f
zaraknyu <razenxizu@gmail.com>
2026-03-28 20:41:30 +0000

    mumba: Drop firmware

--- Android.mk
+++ Android.mk
@@ -3,15 +3,3 @@
 #
 
 LOCAL_PATH := $(call my-dir)
-
-ifeq ($(TARGET_DEVICE),mumba)
-
-$(call add-radio-file-sha1-checked,radio/bluetooth.img,3ece322ce0560a2c8f846b077838f9e41b0baf21)
-$(call add-radio-file-sha1-checked,radio/bootloader.img,46169566f34cd63a33062081bbe8c892645e9355)
-$(call add-radio-file-sha1-checked,radio/dsp.img,d1b389cc099de1a22d0b62b606578008c0131c8d)
-$(call add-radio-file-sha1-checked,radio/fsg.img,6e4603aeb81c18f65680ff09c43b0056baba9856)
-$(call add-radio-file-sha1-checked,radio/gpt.img,6a09235dfb4df2e558e7e67d3e8ec0c86fdcc240)
-$(call add-radio-file-sha1-checked,radio/logo.img,4d95f0288ebc9a85bbbe8e7cef29d080eadbf108)
-$(call add-radio-file-sha1-checked,radio/modem.img,22c4fa735dbb435f76b9f36fb4c5df57a438e0ca)
-
-endif
--- BoardConfigVendor.mk
+++ BoardConfigVendor.mk
@@ -1,12 +1,3 @@
 #
 # Automatically generated file. DO NOT MODIFY
 #
-
-AB_OTA_PARTITIONS += \
-    bluetooth \
-    bootloader \
-    dsp \
-    fsg \
-    gpt \
-    logo \
-    modem
